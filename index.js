a7ace7ae4437b3fb6723af8b3456947b9663960a
Roberto Paes <robertocpaes@gmail.com>
2023-01-07 20:29:47 -0300

    start repo

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const si = require('systeminformation');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+});
+
+io.on('connection', (socket) => {
+    console.log("connected.")
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+    setInterval(function() {
+        si.mem().then(data => {
+            socket.emit("memory", data);
+        })
+        si.processes().then(data => {
+            socket.emit("processes", data);
+        })
+        si.dockerInfo().then(data => {
+            // console.log("docker", data)
+      //      socket.emit("processes", data);
+        })
+      }, 10000)
+});
+  
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
